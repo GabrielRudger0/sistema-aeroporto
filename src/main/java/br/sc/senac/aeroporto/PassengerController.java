3c1e3e31688dbc34f3217e2a428012e0836f3b1c
ChristianSardo <christian.sardo@alunos.sc.senac.br>
2020-05-14 15:43:02 -0300

    Passenger Controller DTO e Entity

--- src/main/java/br/sc/senac/aeroporto/PassengerController.java
+++ src/main/java/br/sc/senac/aeroporto/PassengerController.java
@@ -1,5 +1,15 @@
 package br.sc.senac.aeroporto;
 
+import br.sc.senac.aeroporto.PassengerDTO;
+import br.sc.senac.aeroporto.PassengerEntity;
+
 public class PassengerController {
 
+
+
+private static void updateEntityFromDTO(final PassengerDTO PassengerDTO, final PassengerEntity PassengerEntity) {
+	PassengerEntity.setName(PassengerDTO.getName());
+	PassengerEntity.setEmail(PassengerDTO.getEmail());
+	PassengerEntity.setDate(PassengerDTO.getBirthDate());
+}
 }
